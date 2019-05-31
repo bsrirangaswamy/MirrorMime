5e460fc065374325db994f3b3f1c91ead9a914fa
bsrirangaswamy <30527688+bsrirangaswamy@users.noreply.github.com>
2019-05-30 20:22:53 -0400

    Image Picker
    => Created a image picker to choose an image from the photos library or camera.

---
+++ MirrorMime/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  MirrorMime
+//
+//  Created by Balakumaran Srirangaswamy on 5/30/19.
+//  Copyright © 2019 Bala. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    public var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
--- MirrorMime/ViewController.swift
+++ MirrorMime/ViewController.swift
@@ -7,14 +7,110 @@
 //
 
 import UIKit
+import Photos
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var takePictureButton: UIButton!
+    
+    private var imagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pictureImageView.layer.cornerRadius = pictureImageView.frame.height/2
     }
+    
+    @IBAction func takePictureButtonPressed(_ sender: UIButton) {
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = false
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        if let cameraAction = self.createAction(title: "kTakePicture".localized, sourceType: .camera) {
+            alertController.addAction(cameraAction)
+        }
+        if let photoLibAction = self.createAction(title: "kPhotoLibrary".localized, sourceType: .photoLibrary) {
+            alertController.addAction(photoLibAction)
+        }
+        alertController.addAction(UIAlertAction(title: "kCancel".localized, style: .cancel, handler: nil))
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = sender
+            alertController.popoverPresentationController?.sourceRect = sender.bounds
+            alertController.popoverPresentationController?.permittedArrowDirections = [.down, .up]
+        }
+        
+        self.present(alertController, animated: true)
+    }
+    
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let pickedImage = info[.originalImage] as? UIImage else {
+            return
+        }
+        pictureImageView.image = pickedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+    
+    func createAction(title: String, sourceType: UIImagePickerController.SourceType) -> UIAlertAction? {
+        guard UIImagePickerController.isSourceTypeAvailable(sourceType) else {
+            return nil
+        }
+        let alertAction = UIAlertAction(title: title, style: .default) { [unowned self] (_) in
+            self.checkPermission(sourceType: sourceType, completion: { [unowned self] (isAccessGranted) in
+                if isAccessGranted {
+                    self.imagePickerController.sourceType = sourceType
+                    self.present(self.imagePickerController, animated: true)
+                }
+            })
+        }
+        return alertAction
+    }
+    
+    func checkPermission(sourceType: UIImagePickerController.SourceType, completion: @escaping (_ isPermissionGranted: Bool) -> Void) {
+        switch sourceType {
+        case .camera:
+            let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+            if cameraAuthorizationStatus == .authorized {
+                completion(true)
+            } else if cameraAuthorizationStatus == .notDetermined {
+                AVCaptureDevice.requestAccess(for: .video) { (isAccessGranted) in
+                    if isAccessGranted {
+                        print("Camera access granted")
+                        completion(true)
+                    } else {
+                        completion(false)
+                    }
+                }
+            } else {
+                completion(false)
+            }
+            break
+        case .photoLibrary:
+            let photoLibAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+            if photoLibAuthorizationStatus == .authorized {
+                completion(true)
+            } else if photoLibAuthorizationStatus == .notDetermined {
+                PHPhotoLibrary.requestAuthorization { (requestAuthStatus) in
+                    if requestAuthStatus == .authorized {
+                        print("Photo library access granted")
+                        completion(true)
+                    } else {
+                        completion(false)
+                    }
+                }
+            }
+        default:
+            print("Source type undefined")
+            completion(false)
+        }
+    }
 }
 
